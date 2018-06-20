42325aacb30a4068d18008d75ea1f19e4d6788cb
ondercalbay <ondercalbay.com>
2018-06-20 14:59:02 +0300

    areas

---
+++ Eticaret.WebUI/Areas/Yonetim/YonetimAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Eticaret.WebUI.Areas.Yonetim
+{
+    public class YonetimAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Yonetim";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Yonetim_default",
+                "Yonetim/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Eticaret.WebUI.Areas.Yonetim.Controllers" }
+            );
+        }
+    }
+}

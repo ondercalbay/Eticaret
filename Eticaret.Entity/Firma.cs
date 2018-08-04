1413c7a94ee01486b78c8e8e6b78771735aaece9
ondercalbay <ondercalbay.com>
2018-08-04 16:22:39 +0300

    d

---
+++ Eticaret.Entity/Firma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticaret.Entity
+{
+    [Table("Kategoriler", Schema = "Eticaret")]
+    public class Firma : BaseEntity
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Adi { get; set; }
+
+        [MaxLength(100)]
+        public string LogoUrl { get; set; }
+
+        [MaxLength(100)]
+        public string Telefon { get; set; }
+        [MaxLength(100)]
+        public string Faks { get; set; }
+
+        [MaxLength(200)]
+        public string EPosta { get; set; }
+
+    }
+}
